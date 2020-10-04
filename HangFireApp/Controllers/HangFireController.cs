110a72b852a9ab6a0aa12747d0d34b501c252d61
Ikram Niazi <iniazi@eshopworld.com>
2020-10-04 16:54:40 +0100

    Initial commit

---
+++ HangFireApp/Controllers/HangFireController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using INService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace HangFireApp.Controllers
+{
+    public class HangFireController : Controller
+    {
+        private readonly IINService _inService;
+        public HangFireController(IINService inservice)
+        {
+            this._inService = inservice;
+        }
+        [Route("Api/HangFire")]
+        public IActionResult Index()
+        {
+            RecurringJob.AddOrUpdate("TestJob1", () => this._inService.RunTask(), Cron.Minutely);
+            return Ok();
+        }
+    }
+}
---
+++ INService/Services/INService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INService.Services
+{
+    public interface IINService
+    {
+        void RunTask();
+    }
+    public class INService : IINService
+    {
+        public void RunTask()
+        {
+            Console.WriteLine("This task is run from INService");
+        }
+    }
+}
